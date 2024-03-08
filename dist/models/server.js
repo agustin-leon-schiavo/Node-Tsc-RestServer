f0f0f238395c39000ed818868b95026df50e6fd1
agustin-leon-schiavo <agustinleonschiavo@gmail.com>
2024-03-08 17:16:11 -0300

    primer commit

---
+++ dist/models/server.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const usuario_routes_1 = __importDefault(require("../routes/usuario.routes"));
+const cors_1 = __importDefault(require("cors"));
+class Server {
+    constructor() {
+        this.apiPaths = {
+            usuarios: '/api/usuarios'
+        };
+        this.app = (0, express_1.default)();
+        this.port = process.env.PORT || '8000';
+        // Métodos iniciales
+        this.middlewares();
+        this.routes();
+    }
+    middlewares() {
+        // CORS
+        this.app.use((0, cors_1.default)());
+        // Lectura del body
+        this.app.use(express_1.default.json());
+        // Carpeta pública
+        this.app.use(express_1.default.static('public'));
+    }
+    routes() {
+        this.app.use(this.apiPaths.usuarios, usuario_routes_1.default);
+    }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto ' + this.port);
+        });
+    }
+}
+exports.default = Server;
+//# sourceMappingURL=server.js.map
---
+++ models/server.ts
@@ -0,0 +1,52 @@
+import express, { Application } from "express";
+import userRoutes from '../routes/usuario.routes';
+import cors from 'cors';
+
+
+class Server {
+
+    private app: Application;
+    private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
+
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT || '8000';
+
+        // Métodos iniciales
+        this.middlewares();
+        this.routes();
+    }
+
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura del body
+        this.app.use( express.json() );
+
+        // Carpeta pública
+        this.app.use( express.static('public') );
+    }
+
+
+    routes() {
+        this.app.use( this.apiPaths.usuarios, userRoutes )
+    }
+
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto ' + this.port);  
+        })
+    }
+
+}    
+
+
+export default Server;
